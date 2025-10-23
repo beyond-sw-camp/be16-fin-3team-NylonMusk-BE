2ef32573e0775b9b866aaa195f6500ce9d097830
Hyeseong Bak <hsbak.business@gmail.com>
2025-10-24 02:19:33 +0900

    Feat: 스케줄링 기능 활성화
    - @EnableScheduling 어노테이션 추가로 스케줄 작업 지원
    - Spring의 스케줄링 기능을 통한 자동화 작업 구현 가능
    - 주기적 실행이 필요한 배치 작업 및 모니터링 기능 활성화

--- mkx-platform/src/main/java/com/beyond/MKX/MkxPlatformApplication.java
+++ mkx-platform/src/main/java/com/beyond/MKX/MkxPlatformApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableConfigurationProperties(AuthCookieProperties.class)
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class MkxPlatformApplication {
 
 	public static void main(String[] args) {

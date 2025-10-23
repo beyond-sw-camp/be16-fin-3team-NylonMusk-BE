4444f39ab51ca0e7b033293efddb4e1c893093dd
Hyeseong Bak <hsbak.business@gmail.com>
2025-10-24 02:11:42 +0900

    Feat: 분기별 제출 서류 관리 Enum 정의
    - SubmissionType으로 제출 서류 유형 정의 (재무제표, 감사보고서 등)
    - SubmissionStatus로 제출 상태 관리 (대기, 제출완료, 검토중 등)
    - 분기별 제출 프로세스의 상태 추적을 위한 기반 제공

---
+++ mkx-platform/src/main/java/com/beyond/MKX/domain/delisting/entity/SubmissionType.java
@@ -0,0 +1,8 @@
+package com.beyond.MKX.domain.delisting.entity;
+
+public enum SubmissionType {
+    FINANCIAL_STATEMENT,    // 재무제표
+    AUDIT_REPORT,          // 감사보고서
+    BUSINESS_REPORT,       // 사업보고서
+    QUARTERLY_REPORT       // 분기보고서
+}

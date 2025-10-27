5a45261264c4289cb68366e0dd1276fde3d13b2b
Hyeseong Bak <hsbak.business@gmail.com>
2025-10-27 20:05:32 +0900

    Refactor: CommonDTO 제네릭 타입 적용 및 어노테이션 수정
    - CommonDTO에 제네릭 타입 T 적용으로 유연성 강화
    - 불필요한 Object 필드 제거 및 NoArgsConstructor 추가
    - 코드 일관성 및 Lombok 어노테이션 정리

--- mkx-platform/src/main/java/com/beyond/MKX/common/apiResponse/CommonDTO.java
+++ mkx-platform/src/main/java/com/beyond/MKX/common/apiResponse/CommonDTO.java
@@ -5,13 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Builder
 @Data
-public class CommonDTO {
+public class CommonDTO<T> {
     private String status_message;
     private Integer status_code;
-    private Object result;
+    private T result;
 }

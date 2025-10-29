aa53bca0dfd9c6c0c945a3d0c2ba3e255e96fbce
jinnn12 <prohh12@naver.com>
2025-10-29 10:14:50 +0900

    Feat: 종목 즐겨찾기 Service 구현
    - StockFavoritesRepository, MemberRepository, StockRepository를 활용하여 stock, member 조회 후 favoritesRepository에 StockFavorites 객체 저장
    - List 활용하여 memberId로 즐겨찾기한 종목 조회

---
+++ mkx-platform/src/main/java/com/beyond/MKX/domain/stockfavorite/service/StockFavoritesService.java
@@ -0,0 +1,48 @@
+package com.beyond.MKX.domain.stockfavorite.service;
+
+import com.beyond.MKX.domain.member.entity.Member;
+import com.beyond.MKX.domain.member.repository.MemberRepository;
+import com.beyond.MKX.domain.stock.entity.Stock;
+import com.beyond.MKX.domain.stock.repository.StockRepository;
+import com.beyond.MKX.domain.stockfavorite.entity.StockFavorites;
+import com.beyond.MKX.domain.stockfavorite.repository.StockFavoritesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StockFavoritesService {
+    private final StockFavoritesRepository favoritesRepository;
+    private final StockRepository stockRepository;
+    private final MemberRepository memberRepository;
+
+    public void addFavorite(UUID memberId, UUID stockId) {
+        if (favoritesRepository.existsByMember_IdAndStock_Id(memberId, stockId)) {
+            throw new IllegalArgumentException("이미 즐겨찾기 되어 있는 종목입니다.");
+        }
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("회원 없음"));
+
+        Stock stock = stockRepository.findById(stockId)
+                .orElseThrow(() -> new IllegalArgumentException("종목 없음"));
+
+        favoritesRepository.save(StockFavorites.builder()
+                .member(member)
+                .stock(stock)
+                .build());
+    }
+
+    public void removeFavorite(UUID memberId, UUID stockId) {
+        favoritesRepository.deleteByMember_IdAndStock_Id(memberId, stockId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<StockFavorites> getFavorites(UUID memberId) {
+        return favoritesRepository.findAllByMember_Id(memberId);
+    }
+}

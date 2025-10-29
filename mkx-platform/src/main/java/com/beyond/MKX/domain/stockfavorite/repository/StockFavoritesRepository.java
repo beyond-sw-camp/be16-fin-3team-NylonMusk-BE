325baf1793edc1bd5d3ce4f2a495ce923361ed93
jinnn12 <prohh12@naver.com>
2025-10-29 10:08:11 +0900

    Feat: 종목 즐겨찾기 Repository 구현
    - 회원 아이디 & 종목 아이디로 즐겨찾기 여부 true/false 분기처리
    - 회원 아이디 & 종목 아이디로 즐겨찾기 삭제
    - 회원 아이디로 즐겨찾기 종목 목록 조회

--- mkx-platform/src/main/java/com/beyond/MKX/domain/stockfavorite/repository/StockFavoritesRepository.java
+++ mkx-platform/src/main/java/com/beyond/MKX/domain/stockfavorite/repository/StockFavoritesRepository.java
@@ -3,8 +3,13 @@ package com.beyond.MKX.domain.stockfavorite.repository;
 import com.beyond.MKX.domain.stockfavorite.entity.StockFavorites;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StockFavoritesRepository extends JpaRepository<StockFavorites, UUID> {
-    
+    boolean existsByMember_IdAndStock_Id(UUID memberId, UUID stockId);
+
+    void deleteByMember_IdAndStock_Id(UUID memberId, UUID stockId);
+
+    List<StockFavorites> findAllByMember_Id(UUID memberId);
 }

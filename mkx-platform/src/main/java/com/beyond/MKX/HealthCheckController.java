b2d14fa9777aa16502f4e8b11a513e5ecd80978e
JeaPple <hyungkey0601@gmail.com>
2025-11-07 20:09:39 +0900

    Chore: 배포테스트

---
+++ mkx-platform/src/main/java/com/beyond/MKX/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.beyond.MKX;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "mkx-platform-service OK";
+    }
+}
